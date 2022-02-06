c4f3fe449745a29b18ffd7dd322fb93cf0fc0ca3
jermysen vendarwan <jermysenvendarwan@gmail.com>
2022-02-06 21:13:09 +0800

    setting up student controller

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -14,7 +14,42 @@ public class Student {
     private String email;
     private Gender gender;
 
+    public Student(Long id, String name, String email, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 }
